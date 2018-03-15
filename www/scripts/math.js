c325d6efbd8518c75f971efb5d5a66d09c4b412f
apizzimenti <pizzimentianthony@gmail.com>
2018-03-15 04:02:26 -0500

    Add mathjax init page.

---
+++ www/scripts/math.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    var config = {
+            tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]]},
+            jax: ["input/TeX", "output/HTML-CSS"],
+            "HTML-CSS": {
+                fonts: ["Latin-Modern"]
+            }
+        };
+
+    MathJax.Hub.Config(config);
+    MathJax.Hub.Typeset("stage");
+}
