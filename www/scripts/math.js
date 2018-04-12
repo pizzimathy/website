da792610e133b17da33317eab201cd21784d2acb
apizzimenti <pizzimentianthony@gmail.com>
2018-04-11 22:41:56 -0500

    If there's any errors typesetting the math, just run it again.

--- www/scripts/math.js
+++ www/scripts/math.js
@@ -1,5 +1,10 @@
 
-module.exports = function() {
+/**
+ * @author Anthony Pizzimenti
+ * @desc Renders display math.
+ * @returns {undefined}
+ */
+function math() {
     var config = {
         tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]]},
         jax: ["input/TeX", "output/HTML-CSS"],
@@ -10,4 +15,11 @@ module.exports = function() {
 
     MathJax.Hub.Config(config);
     MathJax.Hub.Typeset("stage");
-};
+
+    // if there's an erro, just try to typeset it again
+    MathJax.Hub.Register.MessageHook("Math Processing Error",function (message) {
+        MathJax.Hub.Typeset("stage");
+    });
+}
+
+module.exports = math;
